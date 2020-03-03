b1a04dcd7cb9883f38f5929c7fa15b82bca92074
archery2000 <ongyuhann@hotmail.com>
2020-03-03 13:51:42 +0800

    Added build tags to android.golang

--- src/connection/Android.go
+++ src/connection/Android.go
@@ -1,24 +1,30 @@
+// +build aix darwin dragonfly freebsd linux netbsd openbsd solaris
+
 package connection
 
 import (
 	"CZ3004-RPi/src/message"
-	"net"
+	"fmt"
+	"golang.org/x/sys/unix"
 )
 
 // AndroidConnection ...
 
 // NewAndroid ...
-func NewAndroid(c chan message.Request) *Connection {
-	t, e := net.ResolveTCPAddr("tcp4", ":9998")
-	if e != nil {
-		panic(e)
-	}
-	conn, e := net.ListenTCP("tcp", t)
+func NewAndroid(port string, baud uint, toRPi chan message.Request) *Connection {
+	fd, _ := Socket(AF_BLUETOOTH, SOCK_STREAM, BTPROTO_RFCOMM)
+	_ = unix.Bind(fd, &unix.SockaddrRFCOMM{
+		Channel: 1,
+		Addr:    [6]uint8{0, 0, 0, 0, 0, 0}, // BDADDR_ANY or 00:00:00:00:00:00
+	})
+	_ = Listen(fd, 1)
+	nfd, sa, _ := Accept(fd)
+	fmt.Printf("conn addr=%v fd=%d", sa.(*unix.SockaddrRFCOMM).Addr, nfd)
+	Read(nfd, buf)
 	for {
-		actual, e := conn.AcceptTCP()
-		if e != nil {
+		if err != nil {
 			continue
 		}
-		return &Connection{actual, c, message.Android}
+		return &Connection{conn, toRPi, message.Android}
 	}
 }
