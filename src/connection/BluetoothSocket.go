142062e9934959147574c1c6a30c85ccc4d7b692
archery2000 <ongyuhann@hotmail.com>
2020-03-03 17:36:11 +0800

    added print message for fd

--- src/connection/BluetoothSocket.go
+++ src/connection/BluetoothSocket.go
@@ -19,6 +19,7 @@ func NewBluetoothSocket(sockAddr Sockaddr, nfr int) *BluetoothSocket {
 
 func (sock *BluetoothSocket) Read(p []byte) (n int, e error) {
 	n, e = Read(sock.nfr, p)
+	fmt.Printf("n: %d, e: %s", n, e)
 	if e != nil {
 		fmt.Printf("n: %d, e: %s", n, e)
 	}
