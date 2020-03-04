d41aa59996c991200298b537a54aabbe6cba7ed4
seaerchin <44049504+seaerchin@users.noreply.github.com>
2020-03-04 13:56:33 +0800

    hello

--- src/rpi/rpi.go
+++ src/rpi/rpi.go
@@ -4,7 +4,6 @@ import (
 	"CZ3004-RPi/src/handler"
 	"CZ3004-RPi/src/message"
 	"bytes"
-	"os"
 	"strconv"
 )
 
@@ -20,6 +19,7 @@ type RPi struct {
 }
 
 const offset = 1 // byte offset between ard/android message
+const discard = 'x'
 
 // Get is a abstraction of a client submitting a request to rpi
 // this just calls the handler
@@ -61,14 +61,23 @@ func (rpi *RPi) AlgoHandler(r message.Request) {
 
 // AndroidHandler handles incoming misc messages from android conn
 func (rpi *RPi) AndroidHandler(r message.Request) {
-	algoBytes := r.M.Buf.Bytes()
-	algoMessage := message.Message{Buf: bytes.NewBuffer(algoBytes)}
-	os.Stdout.Write(algoBytes)
+	// append \n to exploration/setwaypoint
 	switch r.Header {
 	// implicit assumption to do calibration
 	case message.FastestPathStart:
+		algoBytes := []byte{'\n'}
+		algoBytes = append([]byte(strconv.Itoa(int(message.FastestPathStart))), algoBytes...)
+		algoMessage := message.Message{Buf: bytes.NewBuffer(algoBytes)}
 		rpi.outgoingReceivers[message.Arduino](algoMessage) // only fp start routes to ardu
-	default:
+	case message.ExplorationStart:
+		arduinoBytes := []byte{'\n'}
+		arduinoBytes = append([]byte(strconv.Itoa(int(message.ExplorationStart))), arduinoBytes...)
+		arduinoMessage := message.Message{Buf: bytes.NewBuffer(arduinoBytes)}
+		rpi.outgoingReceivers[message.Algo](arduinoMessage) // exploration start + waypoint start routes to algo
+	case message.SetWaypoint:
+		algoBytes := r.M.Buf.Bytes()
+		algoBytes = append([]byte(strconv.Itoa(int(message.SetWaypoint))), algoBytes...)
+		algoMessage := message.Message{Buf: bytes.NewBuffer(algoBytes)}
 		rpi.outgoingReceivers[message.Algo](algoMessage) // exploration start + waypoint start routes to algo
 	}
 	close(r.Result)
@@ -77,7 +86,16 @@ func (rpi *RPi) AndroidHandler(r message.Request) {
 // ArduinoHandler handles incoming sensor input from arduino conn
 func (rpi *RPi) ArduinoHandler(r message.Request) {
 	// format data here
-	rpi.toAlgo <- r.M // new message with formatted data not r.m
+	var leftShort byte
+	if leftShort, _ = r.M.Buf.ReadByte(); leftShort == discard {
+		leftShort, _ = r.M.Buf.ReadByte()
+	} else {
+		_, _ = r.M.Buf.ReadByte()
+	}
+	algoBytes := append([]byte(strconv.Itoa(int(message.Sensor))), leftShort)
+	algoBytes = append(algoBytes, r.M.Buf.Bytes()...)
+	algoMessage := message.Message{bytes.NewBuffer(algoBytes)}
+	rpi.toAlgo <- algoMessage // new message with formatted data not r.m
 	close(r.Result)
 }
 
